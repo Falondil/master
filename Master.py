3e4adddd968e1e1a0e2e6d9d3c6522fac0f9d912
Falondil <92683955+Falondil@users.noreply.github.com>
2022-02-02 14:19:01 +0100

    Add files via upload

--- Master.py
+++ Master.py
@@ -40,6 +40,28 @@ Te = [10**x for x in logTe] # K
 
 cind = min(range(n-1), key=lambda i: abs(logL[i]))
 
+# Plot luminosity w.r.t. stellar age
+plt.figure()
+plt.semilogy(age[cind:],L[cind:],'-', color = 'y')
+plt.gca().yaxis.set_ticks_position('both')
+plt.title('Change in luminosity')
+plt.xlabel('Age of the Sun [yr]')
+plt.ylabel('Luminosity [L_sun]')
+
+plt.figure()
+plt.plot(age[cind:],L[cind:],'-', color = 'y')
+plt.gca().yaxis.set_ticks_position('both')
+plt.title('Change in luminosity')
+plt.xlabel('Age of the Sun [yr]')
+plt.ylabel('Luminosity [L_sun]')
+
+plt.figure()
+plt.plot(age[102+cind-10:102+cind+10],L[102+cind-10:102+cind+10],linestyle='-',marker='.',color='y')
+plt.gca().yaxis.set_ticks_position('both')
+plt.title('Zoomed change in luminosity')
+plt.xlabel('Age of the Sun [yr]')
+plt.ylabel('Luminosity [L_sun]')
+# [Seff[nb][102+cind] for nb in range(numb)] # for the instellation right after the jump down. Paste this after Seff has been calculated
 
 #----------------------------------PART 3--------------------------------------
 # instellation 
@@ -65,15 +87,26 @@ for nb in range(numb):
         topi += [len(Seff[nb])]
 
 # Plotting instellation at each body w.r.t. stellar age
+plt.figure()
 for nb in range(numb):
-    plt.plot(age[cind:topi[nb]],Seff[nb][cind:topi[nb]],'-.',color=colors[nb],label=bodies[nb])
+    plt.semilogy(age[cind:topi[nb]],Seff[nb][cind:topi[nb]],'-.',color=colors[nb],label=bodies[nb])
+plt.gca().yaxis.set_ticks_position('both')
 plt.title('Change in instellation')
 plt.xlabel('Age of the Sun [yr]')
 plt.ylabel('Instellation received by solar system body [S/S0]')
 plt.legend()
 
 
-#----------------------------------PART 4--------------------------------------
+plt.figure()
+for nb in range(numb):
+    plt.semilogy(age[102+cind-10:102+cind+10],Seff[nb][102+cind-10:102+cind+10],marker='.',linestyle='-',color=colors[nb],label=bodies[nb])
+plt.gca().yaxis.set_ticks_position('both')
+plt.title('Zoomed change in instellation')
+plt.xlabel('Age of the Sun [yr]')
+plt.ylabel('Instellation received by solar system body [S/S0]')
+plt.legend()
+
+#----------------------------------PART 4a-------------------------------------
 # Habitable Zone and time within
 
 # enter polynomial fits for T and S
@@ -97,11 +130,40 @@ leftlimit = [IHZKopp(T) for T in Te[cind:]]
 rightlimit = [OHZKopp(T) for T in Te[cind:]]
 # find indices closest to inner and outer HZ boundary for each body 
 leftdiff = [[Scut[nb][i]-leftlimit[i] for i in range(len(Scut[nb]))] for nb in range(numb)] # differences between current S and S at the boundary (same Te)
-leftindex = [leftdiff[nb].index(list(filter(lambda i: i>0, leftdiff[nb]))[0])+cind for nb in range(numb)] # first element OUTSIDE boundary
+leftindex = [leftdiff[nb].index(list(filter(lambda i: i>0, leftdiff[nb]))[0])+cind for nb in range(numb)] # first element OUTSIDE IHZ
 rightdiff = [[Scut[nb][i]-rightlimit[i] for i in range(len(Scut[nb]))] for nb in range(numb)]
-rightindex = [cind if Scut[nb][0]>rightlimit[0] else rightdiff[nb].index(list(filter(lambda i: i>0, rightdiff[nb]))[0])+cind-1 for nb in range(numb)] 
+rightindex = [cind if Scut[nb][0]>rightlimit[0] else rightdiff[nb].index(list(filter(lambda i: i>0, rightdiff[nb]))[0])+cind-1 for nb in range(numb)] # last point outside OHZ
 # consider case where the body at no point is outside the outer HZ boundary
 # in that case, time in HZ starts counting at current time
+
+inIHZ = [[leftlimit[i]>Scut[nb][i] for i in range(len(Scut[nb]))] for nb in range(numb)]
+inOHZ = [[Scut[nb][i]>rightlimit[i] for i in range(len(Scut[nb]))] for nb in range(numb)]
+# Boolean HZ. perhaps alternate way of getting indices. Does not distinguish IHZ and OHZ. 
+booleanHZ = [[inIHZ[nb][i] and inOHZ[nb][i] for i in range(len(Scut[nb]))] for nb in range(numb)]
+for nb in range(numb): # plotting is for working purposes
+    plt.figure()
+    plt.plot(range(len(leftlimit)), booleanHZ[nb], color = colors[nb], label=bodies[nb])
+    plt.legend()
+    plt.figure()
+    plt.plot(range(len(leftlimit)), inIHZ[nb], color = 'r')
+    plt.plot(range(len(leftlimit)), inOHZ[nb], color = 'b')
+    
+    
+trueindices= [[i for i, x in enumerate(booleanHZ[nb]) if x] for nb in range(numb)]
+truestarts = [[i for i in trueindices[nb] if i-1 not in trueindices[nb]] for nb in range(numb)]
+truestops = [[i for i in trueindices[nb] if i+1 not in trueindices[nb]] for nb in range(numb)]
+trueranges = [[[truestarts[nb][i], truestops[nb][i]] for i in range(len(truestarts[nb]))] for nb in range(numb)]
+
+# preliminary timespan calculation
+approxyearsinHZ = [[age[cind+x[1]]-age[cind+x[0]] for x in trueranges[nb]] for nb in range(numb)]
+approxstrYears = [['0' if x == 0 else "{:.1e}".format(x) for x in y] for y in approxyearsinHZ]
+
+# Instead determine when each boundary is crossed
+IHZtrueind = [[i for i, x in enumerate(inIHZ[nb]) if x] for nb in range(numb)]
+IHZtruestarts = [[i for i in IHZtrueind[nb] if i-1 not in IHZtrueind[nb]] for nb in range(numb)]
+IHZtruestops = [[i for i in IHZtrueind[nb] if i+1 not in IHZtrueind[nb]] for nb in range(numb)]
+
+# FRÅGA SARA VARFÖR DET ÄR ETT STORT HOPP I INDEXET 102+CIND=194
 
 # ADD a polynomial fit between first index outside HZ boundary and first index inside HZ boundary for each of the two boundaries.
 leftS = [Seff[nb][leftindex[nb]-1:leftindex[nb]+1] for nb in range(numb)] # [last S inside IHZ, first S outside IHZ]
@@ -119,8 +181,13 @@ agerightintersect = [np.polyval(rightpolys[nb], OHZKopp(rightTeMean[nb])) for nb
 yearinHZ = [ageleftintersect[nb]-agerightintersect[nb] for nb in range(numb)] # how long are the bodies in the HZ
 strYears = ['0' if x == 0 else "{:.1e}".format(x) for x in yearinHZ] # list of strings, scientific notation 1 decimal
 
+#----------------------------------PART 4b-------------------------------------
+# Habitable Zone plotting
+
+# Base case HZ plot
 plt.figure()
 plt.gca().set_aspect(aspect = 1/5780) # arbitrary
+plt.gca().yaxis.set_ticks_position('both')
 plt.plot(inSlim, Tlist, color='r')
 plt.plot(outSlim, Tlist, color='b')
 plt.axis([1.25, 0.2, 2600, 7200])
@@ -128,24 +195,27 @@ plt.title('Habitable zone and solar system body tracks')
 plt.xlabel('Instellation [S/S0]')
 plt.ylabel('Effective temperature [K]')
 for nb in range(numb):
-    plt.plot(Seff[nb][cind:topi[nb]],Te[cind:topi[nb]],'--',color=colors[nb],label=bodies[nb]+', '+strYears[nb]+' years in HZ') # list comprehension label, add time in HZ
+    plt.plot(Seff[nb][cind:topi[nb]],Te[cind:topi[nb]],linestyle='--',marker='.',color=colors[nb],label=bodies[nb]+', '+strYears[nb]+' years in HZ') # list comprehension label, add time in HZ
     plt.plot(Seff[nb][cind],Te[cind],'x',color='k')
-    plt.plot(Seff[nb][cind:topi[nb]],Te[cind:topi[nb]],'.',color=colors[nb]) # good to see while working
+    # plt.plot(Seff[nb][cind:topi[nb]],Te[cind:topi[nb]],'.',color=colors[nb]) # good to see while working
     plt.legend()
 
+#  SATURN BRIEFLY BOUNCES OUTSIDE OHZ
+# plt.figure()
+# plt.plot(inSlim, Tlist, color='r')
+# plt.plot(outSlim, Tlist, color='b')
+# plt.plot(Seff[1][cind:topi[1]],Te[cind:topi[1]],linestyle='--',marker='.',color=colors[1],label=bodies[1]+', '+strYears[1]+' years in HZ') # list comprehension label, add time in HZ
+# plt.axis([0.4, 0.2, 4300, 4600])
 
-
-# """
-# enter T, S tracks markings each 100 Myr. Can't have markings smaller than smallest
-# age difference between adjacent 'age' list elements. 
-# agedifferences = [age[i]-age[i-1] for i in range(n-1)]
-# """
-# Timestep approach may be unnecessary.
-# timestep = 1e8
-# marknumber = 1+int((age[-1]-age[cind+1])/timestep)
-# timelist = [age[cind+1]+x*timestep for x in range(marknumber)]
-# markedind = [min(range(n-1), key=lambda i: abs(age[i]-timelist[x])) for x in range(marknumber)]
+# Same plot but only including tracks beyond first spike in instellation
+# plt.figure()
+# plt.gca().set_aspect(aspect = 1/5780) # arbitrary
+# plt.plot(inSlim, Tlist, color='r')
+# plt.plot(outSlim, Tlist, color='b')
+# plt.axis([1.25, 0.2, 2600, 7200])
+# plt.title('Habitable zone and solar system body tracks')
+# plt.xlabel('Instellation [S/S0]')
+# plt.ylabel('Effective temperature [K]')
 # for nb in range(numb):
-#     for ind in markedind:
-#         plt.plot(Seff[nb][ind], Te[ind], '.', color=colors[nb])
-        
+#     plt.plot(Seff[nb][topi[nb]:],Te[topi[nb]:],linestyle='--',marker='.',color=colors[nb],label=bodies[nb]+', '+strYears[nb]+' years in HZ') # list comprehension label, add time in HZ
+#     plt.legend()
