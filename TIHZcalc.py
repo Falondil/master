9d96a176037808f484ed7bbf7b624e97d2529bbe
Falondil <92683955+Falondil@users.noreply.github.com>
2022-10-07 10:40:20 +0200

    Add files via upload

--- TIHZcalc.py
+++ TIHZcalc.py
@@ -8,17 +8,15 @@ Created on Wed Dec 15 11:49:25 2021
 import matplotlib.pyplot as plt
 import numpy as np
 
-#a6cee3
-#1f78b4
-#b2df8a
-#33a02c for color template on the r, TIHZ(r) continuous barplot
-
 #----------------------------------PART 1--------------------------------------
 # file reading and data arrangement
 
 fileselect = input('Enter 1, 2, 3 for PARSEC, Dartmouth or MIST solar evolution tracks: ')
 if fileselect == '1':
-    filename = "tracks/suntrack_parcol_Zinit0.01774_etaReimers0.3.dat"
+    filename1 = "tracks/Z0.014Y0.273O_IN0.00OUTA1.74_F7_M1.00.tab" 
+    filename2 = "tracks/Z0.014Y0.273O_IN0.00OUTA1.74_F7_M1.00.TAB.HB"
+    # filename1 = "tracks/Z0.017Y0.279O_IN0.00OUTA1.74_F7_M1.00.TAB"
+    # filename2 = "tracks/Z0.017Y0.279O_IN0.00OUTA1.74_F7_M1.00.TAB.HB"
     solarmodel = 'PARSEC'
 elif fileselect == '2':
     filename1 = "tracks/alpha00_M100_RGB.txt"
@@ -31,25 +29,43 @@ else:
     print('Invalid track choice')
     quit()
 
-# PARSEC concatenation must be done manually on a single track file
+# PARSEC has split RGB and HB-AGB tracks to concatenate
 if fileselect == '1':
-    track = []
-    with open(filename) as f:
+    track1 = []
+
+    with open(filename1) as f:
         for line in f:
             rowlist = line.split()
-            track.append(rowlist)
-    track[0].pop(0) # removes the # from start of column names
-    track.pop(-1)   # removes ['#track', 'terminated'] statement
-        
-    n = len(track)
-    age = [float(track[i][3]) for i in range(1,n)]
-    logL = [float(track[i][5]) for i in range(1,n)]
-    logTe = [float(track[i][6]) for i in range(1,n)]
-    
-    # Instant Helium flash jump to quiescent Helium fusion
-    age1 = age[:195]
-    age2 = [a-(age[195]-age[194]) for a in age[195:]]
-    age = age1+age2
+            track1.append(rowlist)
+            
+    track1.pop(-1) # removes end row 
+
+    n1 = len(track1)
+    age1 = [float(track1[i][2]) for i in range(5,n1)]
+    logL1 = [float(track1[i][4]) for i in range(5,n1)]
+    logTe1 = [float(track1[i][5]) for i in range(5,n1)]
+    n1 = len(age1)
+
+    track2 = []
+
+    with open(filename2) as f:
+        for line in f:
+            rowlist = line.split()
+            track2.append(rowlist)
+            
+    track2.pop(-1) # removes end row 
+            
+    n2 = len(track2)
+    age2 = [float(track2[i][2]) for i in range(5,n2)]
+    logL2 = [float(track2[i][4]) for i in range(5,n2)]
+    logTe2 = [float(track2[i][5]) for i in range(5,n2)]
+    n2 = len(age2)
+
+    n = n1+n2+1
+
+    age = age1+[a+age1[-1] for a in age2]
+    logL = logL1+logL2
+    logTe = logTe1+logTe2
         
 # Dartmouth has split RGB and HB-AGB tracks to concatenate
 elif fileselect == '2':
@@ -60,7 +76,7 @@ elif fileselect == '2':
             rowlist = line.split()
             track1.append(rowlist)
             
-    track1[0].pop(0) # removes the # from start of column names
+    track1[0].pop(0)
         
     n1 = len(track1)
     
@@ -74,8 +90,6 @@ elif fileselect == '2':
         for line in f:
             rowlist = line.split()
             track2.append(rowlist)
-            
-    track2[0].pop(0) # removes the # from start of column names
     
     n2 = len(track2)
     
@@ -96,14 +110,10 @@ else:
         for line in f:
             rowlist = line.split()
             track.append(rowlist)
-    track[0].pop(0) # removes the # from start of column names
-    track.pop(-1)   # removes ['#track', 'terminated'] statement
     
-    n = len(track)
-    
-    age = [float(track[i][0]) for i in range(12,n)]
-    logL = [float(track[i][6]) for i in range(12,n)]
-    logTe = [float(track[i][11]) for i in range(12,n)]
+    age = [float(track[i][0]) for i in range(12,1420)] # end-point chosen such that it ends before post-AGB. Visual choice from HR diagram
+    logL = [float(track[i][6]) for i in range(12,1420)]
+    logTe = [float(track[i][11]) for i in range(12,1420)]
 
     n = len(age)+1
     
@@ -139,6 +149,14 @@ colors = ['xkcd:light brown','xkcd:peach','xkcd:light blue','xkcd:bright blue', 
 distances = [5.2, 9.54, 19.2, 30.0, 39.5] # AU, https://www.jpl.nasa.gov/edu/pdfs/ssbeads_answerkey.pdf
 numb = len(bodies)
 
+barwidth = 0.3
+
+# Uncomment these lines to get sum of TIHZ graph
+# distances = [5+barwidth*r for r in range(int(35/barwidth))]
+# bodies = [str(d) for d in distances]
+# numb = len(distances)
+# colors = ['C'+str(i) for i in range(numb)]
+
 # calculations for each body
 Seff = [[x/d**2 for x in L] for d in distances] # S0
 Scut = [Seff[nb][cind:] for nb in range(numb)] # S0, effective instellations after current time
@@ -154,8 +172,6 @@ for nb in range(numb):
         topval += [max(Scut[nb])] # max value instead if it never exceeds maxS
         topi += [len(Seff[nb])] # entire length should be plotted
 
-
-
 #----------------------------------PART 4a-------------------------------------
 # Habitable Zone and time within
 
@@ -164,32 +180,37 @@ c0, c1, c2, c3, c4 = 1.0140, 8.1774e-5, 1.7063e-9, -4.32415e-12, -6.6462e-16 # m
 d0, d1, d2, d3, d4 = 0.3438, 5.8942e-5, 1.6558e-9, -3.0045e-12, -5.2983e-16 # maximum greenhouse
 e0, e1, e2, e3, e4 = 1.776, 2.136e-4, 2.533e-8, -1.332e-11, -3.097e-15 # recent venus
 f0, f1, f2, f3, f4 = 0.32, 5.547e-5, 1.526e-9, -2.874e-12, -5.011e-16 # early mars
+g0, g1, g2, g3, g4 = 1.0512, 1.3242e-4, 1.5418e-8, -7.9895e-12, -1.8328e-15 # runaway greenhouse
 
 Tlist = list(range(2600,7201,10)) 
-def IHZKopp(T):
+def IHZKopp(T): # moist greenhouse
     Tast = T-5780 # T_asterisk from Kopparapu et al. 2013
     return c0 + c1*Tast + c2*Tast**2 + c3*Tast**3 + c4*Tast**4
 
-def OHZKopp(T):
+def OHZKopp(T): # maximum greenhouse
     Tast = T-5780 # T_asterisk from Kopparapu et al. 2013
     return d0 + d1*Tast + d2*Tast**2 + d3*Tast**3 + d4*Tast**4
 
-def RecentVenus(T):
+def RecentVenus(T): # Recent Venus empiric limit
     Tast = T-5780
     return e0 + e1*Tast + e2*Tast**2 + e3*Tast**3 + e4*Tast**4
 
-def EarlyMars(T):
+def EarlyMars(T): # Early Mars empiric limit
     Tast = T-5780
     return f0 + f1*Tast + f2*Tast**2 + f3*Tast**3 + f4*Tast**4
 
-# choose which HZ boundary definition to use
-# funclist = [IHZKopp, OHZKopp]
-# boundaries = 'Kopp'
-# boundword = 'Conservative'
+def RunawayKopp(T): # runaway greenhouse
+    Tast = T-5780 # T_asterisk from Kopparapu et al. 2013
+    return g0 + g1*Tast + g2*Tast**2 + g3*Tast**3 + g4*Tast**4
 
-funclist = [RecentVenus, EarlyMars]
-boundaries = 'RVEM'
-boundword = 'Optimistic'
+# choose which HZ boundary definition to use
+funclist = [IHZKopp, OHZKopp]
+boundaries = 'Kopp'
+boundword = 'Conservative'
+
+# funclist = [RecentVenus, EarlyMars]
+# boundaries = 'RVEM'
+# boundword = 'Optimistic'
 
 # Plotting instellation at each body w.r.t. stellar age
 plt.figure()
@@ -205,7 +226,7 @@ plt.figure()
 for nb in range(numb):
     plt.semilogy(age[cind:],Seff[nb][cind:],'-',linewidth=2,color=colors[nb],label=bodies[nb])
 plt.gca().yaxis.set_ticks_position('both')
-plt.gca().set_xlim(left= 1.15e10, right= 1.24e10)
+plt.gca().set_xlim(left= age[-1]*0.95, right= age[-1]*1.005)
 plt.semilogy(age[cind:], [funclist[0](T) for T in Te[cind:]], color='r', linewidth=1.5, linestyle='--')
 plt.semilogy(age[cind:], [funclist[1](T) for T in Te[cind:]], color='b', linewidth=1.5, linestyle='--')
 plt.title('Change in instellation ('+solarmodel+')')
@@ -214,8 +235,8 @@ plt.ylabel('Instellation received by solar system body [S/S0]')
 plt.legend()
 plt.savefig('Plots/'+solarmodel+'-instellation-'+boundaries+'.pdf')
 
-inSlim = [IHZKopp(T) for T in Tlist] # inner HZ boundary limit
-outSlim = [OHZKopp(T) for T in Tlist] # outer HZ boundary limit
+inSlim = [funclist[0](T) for T in Tlist] # inner HZ boundary limit
+outSlim = [funclist[1](T) for T in Tlist] # outer HZ boundary limit
 rvSlim = [RecentVenus(T) for T in Tlist] # optimistic inner HZ
 emSlim = [EarlyMars(T) for T in Tlist] # optimistic outer HZ
 
@@ -224,7 +245,7 @@ leftlimit = [funclist[0](T) for T in Te[cind:]]
 rightlimit = [funclist[1](T) for T in Te[cind:]]
 # find indices closest to inner and outer HZ boundary for each body 
 leftdiff = [[Scut[nb][i]-leftlimit[i] for i in range(len(Scut[nb]))] for nb in range(numb)] # differences between current S and S at the boundary (same Te)
-leftindex = [leftdiff[nb].index(list(filter(lambda i: i>0, leftdiff[nb]))[0])+cind for nb in range(numb)] # first element OUTSIDE IHZ
+leftindex = [leftdiff[nb].index(list(filter(lambda i: i>0, leftdiff[nb]))[0])+cind for nb in range(numb)] # first element OUTSIDE IHZ (i.e. first point with too much instellation)
 rightdiff = [[Scut[nb][i]-rightlimit[i] for i in range(len(Scut[nb]))] for nb in range(numb)]
 rightindex = [cind if Scut[nb][0]>rightlimit[0] else rightdiff[nb].index(list(filter(lambda i: i>0, rightdiff[nb]))[0])+cind-1 for nb in range(numb)] # last point outside OHZ
 # consider case where the body at no point is outside the outer HZ boundary
@@ -232,7 +253,6 @@ rightindex = [cind if Scut[nb][0]>rightlimit[0] else rightdiff[nb].index(list(fi
 
 inIHZ = [[leftlimit[i]>Scut[nb][i] for i in range(len(Scut[nb]))] for nb in range(numb)]
 inOHZ = [[Scut[nb][i]>rightlimit[i] for i in range(len(Scut[nb]))] for nb in range(numb)]
-# Boolean HZ. perhaps alternate way of getting indices. Does not distinguish IHZ and OHZ. 
 booleanHZ = [[inIHZ[nb][i] and inOHZ[nb][i] for i in range(len(Scut[nb]))] for nb in range(numb)]
 for nb in range(numb):
     booleanHZ[nb][-1]=False # Stupid way to ensure that the habitablity time calculations work even if you have a track too short to plot all the timespans inside HZ
@@ -260,8 +280,10 @@ plt.loglog(Te[cind:], L[cind:], linestyle='--',marker='.', color = 'gray', mfc =
 plt.loglog([Te[cind+i] for i in anytrue], [L[cind+i] for i in anytrue], '.', color = 'g')
 plt.xlabel('Effective temperature [K]')
 plt.ylabel('Luminosity [L_sun]')
-plt.title('HR-esque diagram ('+solarmodel+')')
+plt.title('HR diagram ('+solarmodel+')')
 plt.gca().invert_xaxis()
+plt.xlim([7200, 2600])
+plt.ylim([0.9*10**0, 10**4])
 plt.legend()
 
 # preliminary timespan calculation
@@ -287,7 +309,7 @@ allyearsinHZ = [[stopagepolyval[nb][i] - startagepolyval[nb][i] for i in range(l
 
 plt.figure()
 for nb in range(numb):
-    plt.bar([nb + 0.25*i for i in range(len(allyearsinHZ[nb]))], allyearsinHZ[nb], width=0.25, color=colors[nb], label=bodies[nb])
+    plt.bar([nb + 0.1666*i for i in range(len(allyearsinHZ[nb]))], allyearsinHZ[nb], width=0.1666, color=colors[nb], label=bodies[nb])
     plt.legend()
 plt.xticks(range(numb), bodies)
 plt.ylabel('timespan [years]')
@@ -324,27 +346,25 @@ for nb in range(numb):
     plt.plot(Seff[nb][cind],Te[cind],'o',color='k')
     plt.legend()
 
-# # Same plot for each planet, but full
-# for nb in range(numb):
-#     plt.figure()
-#     plt.gca().set_aspect(aspect = 1.8/5780) # arbitrary
-#     plt.plot(inSlim, Tlist, color='r')
-#     plt.plot(outSlim, Tlist, color='b')
-#     plt.plot(emSlim, Tlist, color='C0')
-#     plt.plot(rvSlim, Tlist, color='C1')
-#     plt.axis([2, 0.2, 2600, 7200])
-#     plt.title('Habitable zone track of '+bodies[nb]+' ('+solarmodel+')')
-#     plt.xlabel('Instellation [S/S0]')
-#     plt.ylabel('Effective temperature [K]')
-#     plt.plot(Seff[nb][cind:],Te[cind:],linestyle='-',linewidth=1,marker='.',color=colors[nb],label=bodies[nb]+': '+str(strYears[nb])+' years in HZ')
-#     plt.plot(Seff[nb][-1],Te[-1],'x',color='k')
-#     plt.legend(loc='upper right')
-
-
-
 # #----------------------------------EXTRA---------------------------------------
 # # Extra plotting code, must be run manually
-
+if len(distances)>10:
+    mapcolors = ['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c'] 
+    
+    plt.figure()
+    for nb in range(numb):
+        plt.bar(distances[nb], allyearsinHZ[nb][0], width = barwidth, color=mapcolors[0])
+        prevTIHZ = allyearsinHZ[nb][0]
+        for i in range(1, len(allyearsinHZ[nb])):
+            plt.bar(distances[nb], allyearsinHZ[nb][i], width = barwidth, bottom=prevTIHZ, color=mapcolors[1:][(i-1)%(len(mapcolors)-1)])
+            prevTIHZ = prevTIHZ+allyearsinHZ[nb][i]
+    plt.xlabel('Orbiting radius [AU]')
+    plt.ylabel('Timespan inside HZ [yr]')
+    plt.plot([], label="Pass #1", color=mapcolors[0])
+    for i in range(max([len(allyearsinHZ[nb]) for nb in range(numb)])):
+        plt.plot([], label="Pass #"+str(2+i), color=mapcolors[1:][(i-1)%(len(mapcolors)-1)])
+    plt.legend()
+    plt.title('Sum of TIHZ ('+solarmodel+')')
 
 # plt.figure()
 # for nb in range(numb):
@@ -356,7 +376,6 @@ for nb in range(numb):
 # plt.bar(0, height=0, width=0, facecolor='grey', label=solarmodel)
 # plt.bar(0, height = 0, width=0, fill = False, hatch='//', label ='PARSEC')
 # plt.legend()
-
 
 # plt.savefig('Plots/'+boundaries+'-dartmouth+parsec-TIHZ.pdf')
 
