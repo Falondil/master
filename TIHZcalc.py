e2caaee6444911dc5496ec68df314dc8583ffc03
Falondil <92683955+Falondil@users.noreply.github.com>
2022-11-04 13:44:13 +0100

    Add files via upload
    Added secondary y-axis for Earth Oceans Lost units on water-loss plot.

--- TIHZcalc.py
+++ TIHZcalc.py
@@ -44,6 +44,7 @@ if fileselect == '1':
     age1 = [float(x[2]) for x in track1[5:]]
     logL1 = [float(x[4]) for x in track1[5:]]
     logTe1 = [float(x[5]) for x in track1[5:]]
+    PHS1 = [float(x[95]) for x in track1[5:]]
     n1 = len(age1)
 
     track2 = []
@@ -58,6 +59,7 @@ if fileselect == '1':
     age2 = [float(x[2]) for x in track2[5:]]
     logL2 = [float(x[4]) for x in track2[5:]]
     logTe2 = [float(x[5]) for x in track2[5:]]
+    PHS2 = [float(x[95]) for x in track2[5:]]
     n2 = len(age2)
 
     n = n1+n2+1
@@ -65,7 +67,7 @@ if fileselect == '1':
     age = age1+[a+age1[-1] for a in age2]
     logL = logL1+logL2
     logTe = logTe1+logTe2
-        
+    PHS = PHS1+PHS2
     
 # Dartmouth has split RGB and HB-AGB tracks to concatenate
 elif fileselect == '2':
@@ -190,12 +192,14 @@ for nb in range(numb):
 #----------------------------------PART 4a-------------------------------------
 # Habitable Zone and time within
 
-# enter polynomial fits for T and S
+# enter polynomial fits for T and S from Kopp2013
 c0, c1, c2, c3, c4 = 1.0140, 8.1774e-5, 1.7063e-9, -4.32415e-12, -6.6462e-16 # moist greenhouse
 d0, d1, d2, d3, d4 = 0.3438, 5.8942e-5, 1.6558e-9, -3.0045e-12, -5.2983e-16 # maximum greenhouse
-e0, e1, e2, e3, e4 = 1.776, 2.136e-4, 2.533e-8, -1.332e-11, -3.097e-15 # recent venus
-f0, f1, f2, f3, f4 = 0.32, 5.547e-5, 1.526e-9, -2.874e-12, -5.011e-16 # early mars
+e0, e1, e2, e3, e4 = 1.7753, 1.4316e-4, 2.9875e-9, -7.5702e-12, -1.1635e-15 # recent venus, Kopp2014: 1.776, 2.136e-4, 2.533e-8, -1.332e-11, -3.097e-15
+f0, f1, f2, f3, f4 = 0.3179, 5.4513e-5, 1.5313e-9, -2.7786e-12, -4.8997e-16 # early mars, Kopp2014: 0.32, 5.547e-5, 1.526e-9, -2.874e-12, -5.011e-16
 g0, g1, g2, g3, g4 = 1.0512, 1.3242e-4, 1.5418e-8, -7.9895e-12, -1.8328e-15 # runaway greenhouse
+
+# Ramirez2018
 h0, h1, h2, h3, h4 = 0.3050, 2.216e-5, 4.1913e-9, -1.3177e-12, 1.1796e-16 # Methane: 0.1 CH4/CO2. 
 
 Tlist = list(range(2600,7201,10)) 
@@ -224,13 +228,13 @@ def Methane(T): # 0.1 CH4/CO2 from Ramirez&Kaltenegger 2018
     return h0 + h1*Tast + h2*Tast**2 + h3*Tast**3 + h4*Tast**4
 
 # choose which HZ boundary definition to use
-funclist = [RunawayKopp, OHZKopp]
-boundaries = 'Kopp'
-boundword = 'Conservative'
+# funclist = [RunawayKopp, OHZKopp]
+# boundaries = 'Kopp'
+# boundword = 'Conservative'
 
-# funclist = [RecentVenus, EarlyMars]
-# boundaries = 'RVEM'
-# boundword = 'Optimistic'
+funclist = [RecentVenus, EarlyMars]
+boundaries = 'RVEM'
+boundword = 'Optimistic'
 
 # Plotting instellation at each body w.r.t. stellar age
 plt.figure()
@@ -239,7 +243,7 @@ for nb in range(numb):
 plt.gca().yaxis.set_ticks_position('both')
 plt.title('Change in instellation ('+solarmodel+')')
 plt.xlabel('Age of the Sun [yr]')
-plt.ylabel('Instellation received by solar system body [S/S0]')
+plt.ylabel('Instellation received by solar system body '+r'$[S/S_\bigoplus]$')
 plt.legend()
 
 plt.figure()
@@ -251,12 +255,33 @@ plt.semilogy(age[cind:], [funclist[0](T) for T in Te[cind:]], color='r', linewid
 plt.semilogy(age[cind:], [funclist[1](T) for T in Te[cind:]], color='b', linewidth=1.5, linestyle='--')
 plt.title('Change in instellation ('+solarmodel+')')
 plt.xlabel('Age of the Sun [yr]')
-plt.ylabel('Instellation received by solar system body [S/S0]')
+plt.ylabel('Instellation received by solar system body '+r'$[S/S_\bigoplus]$')
 plt.legend()
 
 # HZ boundaries for plotting
 inSlim = [funclist[0](T) for T in Tlist] # inner HZ boundary limit
 outSlim = [funclist[1](T) for T in Tlist] # outer HZ boundary limit
+
+emSlim = [EarlyMars(T) for T in Tlist] # Early Mars instellation limit
+rvSlim = [RecentVenus(T) for T in Tlist] # Recent Venus instellation limit
+moistSlim = [IHZKopp(T) for T in Tlist] # Moist greenhouse instellation limit
+runawaySlim = [RunawayKopp(T) for T in Tlist] # Runaway greenhouse instellation limit
+maxGHSlim = [OHZKopp(T) for T in Tlist] # Maximum greenhouse instellation limit
+
+# Plotting HZ limits devoid of anything else
+plt.figure()
+plt.gca().set_aspect(aspect=(2-0.2)/5780) # arbitrary
+plt.gca().yaxis.set_ticks_position('both')
+plt.plot(rvSlim, Tlist, color='r', linestyle=':', label='Recent Venus')
+plt.plot(runawaySlim, Tlist, color='r', linestyle='--', label='Runaway')
+plt.plot(moistSlim, Tlist, color='r', label='Moist')
+plt.plot(maxGHSlim, Tlist, color='b', label='Maximum')
+plt.plot(emSlim, Tlist, color='b', linestyle=':', label='Early Mars')
+plt.axis([2, 0.2, 2600, 7200])
+plt.title('Conservative and optimistic HZ comparison')
+plt.xlabel('Instellation '+r'$[S/S_\bigoplus]$')
+plt.ylabel('Effective temperature [K]')
+plt.legend(loc='lower left')
 
 # Find the S limit for a given Te from the stellar track
 leftlimit = [funclist[0](T) for T in Te[cind:]]
@@ -351,18 +376,22 @@ for nb in range(numb):
     plt.ylabel('timespan [years]')
     plt.title('Timespans inside the Habitable Zone ('+solarmodel+')')
     plt.legend()
-    plt.savefig('Plots/'+solarmodel+bodies[nb]+'TIHZ'+boundaries+'.pdf')
 
 strYears = [['0' if x == 0 else "{:.1e}".format(x) for x in allyearsinHZ[nb]] for nb in range(numb)] # list of strings, scientific notation 1 decimal
 
 # Water-loss time plot
-plt.figure()
+EarthOceanLossTime = 5e14/(86400*365.241) # from Kasting et al 1993 using mixing ratio f(H20) = 1 (completely water dominated).
+wfig=plt.figure()
+wax=wfig.add_subplot(111)
 for nb in range(numb):
     plt.bar([nb + 0.1666*i for i in range(len(waterlosstimes[nb]))], waterlosstimes[nb], width=0.1666, color=colors[nb])
-plt.axhline(2e7, linestyle = '--', color='C0', label='1 Earth Ocean lost')
-plt.axhline(4e7, linestyle = '-', color='C0', label='2 Earth Oceans lost')
-plt.legend()
 plt.xticks(range(numb), bodies)
+wax2 = wax.secondary_yaxis('right', functions=(lambda x: x/EarthOceanLossTime, lambda x: x*EarthOceanLossTime))
+wax2.set_ylabel('Earth Ocean(s) lost')
+# plt.axhline(2e7, linestyle = '--', color='C0', label='1 Earth Ocean lost')
+# plt.axhline(4e7, linestyle = '-', color='C0', label='2 Earth Oceans lost')
+plt.legend()
+
 plt.ylabel('timespan [years]')
 plt.title('Waterloss timespans ('+solarmodel+')')
 
@@ -378,7 +407,7 @@ plt.plot(inSlim, Tlist, color='r')
 plt.plot(outSlim, Tlist, color='b')
 plt.axis([Smax, 0.2, 2600, 7200])
 plt.title('First pass through '+boundword+' HZ ('+solarmodel+')')
-plt.xlabel('Instellation [S/S0]')
+plt.xlabel('Instellation '+r'$[S/S_\bigoplus]$')
 plt.ylabel('Effective temperature [K]')
 for nb in range(numb):
     plt.plot(Seff[nb][cind:n1],Te[cind:n1],linestyle='-',linewidth=1,marker='.',color=colors[nb],label=bodies[nb]) # n1 is index for last point before helium flash
@@ -387,6 +416,9 @@ for nb in range(numb):
 
 # #----------------------------------EXTRA---------------------------------------
 # # Extra plotting code, must be run manually
+
+
+# SUM OF TIHZ GRAPH. Might want to split it in two where the colors are not present in the first. 
 if len(distances)>10:
     mapcolors = ['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c'] 
     
@@ -404,8 +436,7 @@ if len(distances)>10:
         plt.plot([], label="Pass #"+str(2+i), color=mapcolors[1:][(i-1)%(len(mapcolors)-1)])
     plt.xlim([distances[0]*0.75, distances[-1]*1.05])
     legend1 = plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0.)
-    plt.title('Sum of TIHZ ('+solarmodel+')')
-    
+    plt.title('Sum of '+boundword+' TIHZ ('+solarmodel+')')
     
     # add vlines at orbital distances of the planets
     planets = ['Jupiter','Saturn','Uranus','Neptune', 'Pluto'] # name of the bodies
@@ -421,7 +452,6 @@ if len(distances)>10:
     plt.legend(handles=linelist, bbox_to_anchor=(1.005, 0), loc='lower left')
     plt.gca().add_artist(legend1)    
     
-    
 
 # plt.figure()
 # for nb in range(numb):
@@ -434,3 +464,7 @@ if len(distances)>10:
 # plt.bar(0, height = 0, width=0, fill = False, hatch='//', label ='PARSEC')
 # plt.legend()
 
+
+
+# How to save figures
+# plt.savefig('Plots/blablabla.pdf')
